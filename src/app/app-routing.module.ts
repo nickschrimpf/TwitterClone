84da020c13efeac24356266b648fa7b107d38d26
Nicholas Schrimpf <nickschrimpf@Nicholass-MacBook-Pro.local>
2022-06-01 09:26:45 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TimelineComponent } from './timeline/timeline.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:TimelineComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { NewTweetComponent } from './timeline/new-tweet/new-tweet.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent,
+    NewTweetComponent,
   ],
   imports: [
     BrowserModule,
