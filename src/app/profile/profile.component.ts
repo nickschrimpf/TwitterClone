8cb7d29466c958f808de3129c4522de46308c835
nickschrimpf <nicholas.schrimpf@mac.com>
2022-10-06 09:20:15 -0600

    outline icons and created a link to the tweets profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
-import { Subscription } from 'rxjs';
+import {  Subscription } from 'rxjs';
 import { UserService } from '../auth/user.service';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -13,36 +13,49 @@ export class ProfileComponent implements OnInit, OnDestroy {
   routeSub:Subscription
   userProfileSub: Subscription;
   userProfile
+  profileOwner = false
   constructor(
     private userServ:UserService,
     private route:ActivatedRoute,
-    private router:Router
+    private router:Router,
+    private location:Location
   ) { }
 
   ngOnInit(): void {
+  
     this.profileUser = this.route.snapshot.params['id']
     this.routeSub = this.route.params
       .subscribe((params:Params) => {
           const currenUserFlutterName = params['id']
           this.userServ.getUserProfilebyFN(currenUserFlutterName).subscribe(user => {
             this.profileUser = user[0]
-            console.log(this.profileUser)
+             this.userProfileSub = this.userServ.userProfile.subscribe(user => {
+              this.userProfile = user
+              if(this.profileUser && this.userProfile){
+                if( this.userProfile.id === this.profileUser.id){
+                  this.profileOwner = true;
+                }
+              }
+              }
+            )
           })
           
         }
       )
-    this.userProfileSub = this.userServ.userProfile.subscribe(user => {
-      this.userProfile = user
-      console.log(this.userProfile)
-      }
-    )
+    
   }
+ 
+ 
   onEdit(){
     console.log(this.profileUser.flutterName)
     this.router.navigate(['editprofile'],{relativeTo:this.route})
   }
+  onBack(){
+    this.location.back()
+  }
   ngOnDestroy(): void {
-   this.routeSub.unsubscribe()
+    this.userProfileSub.unsubscribe()
+    this.routeSub.unsubscribe()
   }
 
 }
