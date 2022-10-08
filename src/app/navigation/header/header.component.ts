1ac4e92664ee35757a29783ad9046957d27d12d4
nickschrimpf <nicholas.schrimpf@mac.com>
2022-10-08 00:29:36 -0600

    adding user profile photo instead of place holder

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
 import { Location } from '@angular/common';
+import { UserService } from 'src/app/auth/user.service';
 
 
 @Component({
@@ -11,14 +12,21 @@ import { Location } from '@angular/common';
 })
 export class HeaderComponent implements OnInit {
   @Output()  sidenavToggle = new EventEmitter<void>()
-  
+  userProfile
+  isLoading = false
 
-  constructor(private authService:AuthService,) { 
+  constructor(private authService:AuthService,private userServ:UserService) { 
   
   }
 
   ngOnInit(): void {
+    this.isLoading = true
+    this.userServ.userProfile.subscribe(data => {
+      this.userProfile = data;
+      this.isLoading = false;
+    })
   }
+
   logOut(){
     this.authService.logOut()
   }
